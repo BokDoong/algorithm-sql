55ad77006f259dd3264579a82ac22c4f1bf1b7f4
BokDoong <102129660+BokDoong@users.noreply.github.com>
2023-01-27 12:46:24 +0900

    Add files via upload

---
+++ NM/test_15664.java
@@ -0,0 +1,59 @@
+package BaekJoon_Study.NM;
+
+import java.util.*;
+
+public class test_15664 {
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] result = new int[10];
+    public static boolean[] check = new boolean[10];
+    public static int[] test;
+    public static int N;
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        //input
+        String[] str = sc.nextLine().split(" ");
+        N = Integer.parseInt(str[0]);
+        int M = Integer.parseInt(str[1]);
+
+        test = new int[N];
+        str = sc.nextLine().split(" ");
+        for (int i = 0; i < N; i++)
+            test[i] = Integer.parseInt(str[i]);
+
+        //main
+        Arrays.sort(test);
+        dfs(0,0, M);
+
+        //output
+        System.out.println(sb);
+    }
+
+    public static void dfs(int now, int idx, int size) {
+        if(idx == size){
+            for (int i = 0; i < size; i++) {
+                sb.append(result[i]);
+                if(i!=size-1)
+                    sb.append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = 0;
+        for (int i = now; i < N; i++) {
+            if(check[i])
+                continue;
+
+            if (before != test[i]) {
+                check[i] = true;
+                result[idx]=test[i];
+                before = test[i];
+                dfs(i,idx + 1, size);
+                check[i] = false;
+            }
+        }
+
+    }
+}
