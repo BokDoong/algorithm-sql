6847a793a05569ec9ef2fcf212bb20f499f3dde7
bokdoong <102129660+BokDoong@users.noreply.github.com>
2023-05-16 11:51:15 +0900

    Add files via upload

--- bfs/test_1697.java
+++ bfs/test_1697.java
@@ -6,54 +6,50 @@ import java.util.Scanner;
 
 public class test_1697 {
 
-    static int N, K;
+    static int K;
     static int[] visited;
-    static Queue<Integer> queue;
-
-    static int bfs(int now){
-        queue.offer(now);
-
-        while (!queue.isEmpty()) {
-            int num = queue.poll();
-
-            if (num == K) {
-                return visited[num];
-            }
-
-            //+1
-            if(num+1 <= 100000 && visited[num+1] == 0) {
-                visited[num+1] = visited[num] + 1;
-                queue.add(num + 1);
-            }
-
-            //-1
-            if(num-1 >= 0 &&visited[num-1] == 0) {
-                visited[num-1] = visited[num] + 1;
-                queue.add(num - 1);
-            }
-
-            //*2
-            if(num*2 <= 100000 && visited[num*2] == 0) {
-                visited[num*2] = visited[num]+1;
-                queue.add(num * 2);
-            }
-        }
-
-        return -1;
-    }
 
     public static void main(String[] args) {
-
         //input
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
+        int N = sc.nextInt();
         K = sc.nextInt();
 
         //main
         visited = new int[100001];
-        queue = new LinkedList<>();
+        bfs(N);
 
         //output
-        System.out.println(bfs(N));;
+        System.out.println(visited[K]);
+    }
+
+    public static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+            if(now == K)
+                return;
+
+            int nx = 0;
+            for (int i = 0; i < 3; i++) {
+                if (i == 0)
+                    nx = now + 1;
+                else if (i == 1)
+                    nx = now - 1;
+                else
+                    nx = now*2;
+
+                if(nx > 100000 || nx < 0)
+                    continue;
+
+                if (visited[nx] == 0) {
+                    visited[nx] = visited[now] + 1;
+                    queue.add(nx);
+                }
+            }
+
+        }
     }
 }
