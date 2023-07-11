0b017662564a5cffa189eb178c02e2cd3f8e4a6d
bokdoong <102129660+BokDoong@users.noreply.github.com>
2023-07-11 12:18:43 +0900

    Add files via upload

--- datastructure1/test_10828.java
+++ datastructure1/test_10828.java
@@ -1,44 +1,45 @@
-package datastructure1;
-import java.util.*;
-import java.io.*;
+package BaekJoon_Study.datastructure1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class test_10828 {
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			
-		Stack<Integer> stack = new Stack<>();
-		int n = Integer.parseInt(br.readLine());
-		while(n-- > 0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String x = st.nextToken();
-			
-			switch(x) {
-			case "push":
-				stack.push(Integer.parseInt(st.nextToken()));
-				break;
-			case "pop":
-				if (stack.isEmpty())
-					System.out.println(-1);
-				else
-					System.out.println(stack.pop());
-				break;
-			case "size":
-				System.out.println(stack.size());
-				break;
-			case "empty":
-				if (stack.isEmpty())
-					System.out.println(1);
-				else
-					System.out.println(0);
-				break;
-			case "top":
-				if (stack.isEmpty())
-					System.out.println(-1);
-				else
-					System.out.println(stack.peek());
-				break;
-			}
-		}
-		
-	}
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            String[] com = br.readLine().split(" ");
+            switch (com[0]) {
+                case "push":
+                    stack.push(Integer.parseInt(com[1]));
+                    break;
+                case "pop":
+                    if (stack.isEmpty())
+                        sb.append(-1).append("\n");
+                    else
+                        sb.append(stack.pop()).append("\n");
+                    break;
+                case "top":
+                    sb.append(stack.peek()).append("\n");
+                    break;
+                case "size":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "empty":
+                    int res = stack.isEmpty() ? 1 : 0;
+                    sb.append(res).append("\n");
+                    break;
+            }
+        }
+
+        System.out.print(sb);
+    }
 }
