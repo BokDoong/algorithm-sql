bbee96d19571a4864c225f77056a690f63b0f8aa
bokdoong <102129660+BokDoong@users.noreply.github.com>
2024-01-11 14:15:14 +0900

    Update Programmers_68645.java

--- 배열/Programmers_68645.java
+++ 배열/Programmers_68645.java
@@ -1,8 +1,53 @@
 package Book;
 
 public class Programmers_68645 {
-    
-    private static int[] solution(int n) {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // int[] result = my_solution(n);
+        int[] result = book_solution(n);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+
+    private static int[] book_solution(int n) {
+        // dx, dy
+        int[] dx = {0, 1, -1};
+        int[] dy = {1, 0, -1};
+
+        // 숫자판, 사용할 변수
+        int[][] board = new int[n][n];
+        int value = 1;
+        int x = 0, y = 0, d = 0;
+
+        while (true) {
+            // 1. 값 넣기 -> 좌표 이동
+            board[y][x] = value++;
+            int ny = y + dy[d];
+            int nx = x + dx[d];
+
+            // 2. 다음 칸에 숫자가 있거나 범위 벗어나면 방향 바꾸기
+            if (nx == -1 || ny == -1 || nx == n || ny == n || board[ny][nx] != 0) {
+                d = (d + 1) % 3;
+                ny = y + dy[d];
+                nx = x + dx[d];
+
+                // 3. 한번 더 체크해서 break
+                if (nx == -1 || ny == -1 || nx == n || ny == n || board[ny][nx] != 0)
+                    break;
+            }
+
+            y = ny;
+            x = nx;
+        }
+
+        return convertToResult(n, board);
+    }
+
+    private static int[] my_solution(int n) {
         // 숫자판
         int[][] board = new int[n][n];
         // 사용할 값
@@ -29,6 +74,10 @@ public class Programmers_68645 {
             end.x -= 2; end.y -= 1;
         }
 
+        return convertToResult(n, board);
+    }
+
+    private static int[] convertToResult(int n, int[][] board) {
         // 일차원 배열 만들기
         int order = 0;
         int[] result = new int[n * (n + 1) / 2];
@@ -37,7 +86,6 @@ public class Programmers_68645 {
                 result[order++] = board[i][j];
             }
         }
-
         return result;
     }
 
