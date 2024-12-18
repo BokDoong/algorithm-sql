a6f0f6b1a3b280570094c05032d8b28f423b4976
bokdoong <102129660+BokDoong@users.noreply.github.com>
2024-12-18 20:26:54 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,18 +1,19 @@
-with recursive GenerationCTE as (
-    # 초기 데이터
-    select id, parent_id, 1 as generation
+# 세대별 대장균, 전 세대의 부모 대장균 정보
+with recursive generation_infos as (
+    # 1세대
+    select 1 as generation, id, parent_id
     from ecoli_data
     where parent_id is null
     
     union all
     
-    # 재귀 데이터
-    select e.id, e.parent_id, g.generation + 1 as generation
-    from ecoli_data e
-    join GenerationCTE as g on e.parent_id = g.id
+    # 다음 세대
+    select generation_infos.generation + 1, ecoli_data.id, ecoli_data.parent_id
+    from ecoli_data
+    join generation_infos on generation_infos.id = ecoli_data.parent_id
 )
-select count(*) as COUNT, generation as GENERATION
-from GenerationCTE
-where id not in (select distinct parent_id from GenerationCTE where parent_id is not null)
+select count(*) as COUNT, GENERATION
+from generation_infos
+where id not in (select parent_id from generation_infos where parent_id is not null)
 group by generation
-order by GENERATION
+order by generation
