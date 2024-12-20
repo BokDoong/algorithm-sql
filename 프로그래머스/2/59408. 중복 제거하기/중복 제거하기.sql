4648f5d3106a3d3696bc6a68293e51e6582e84c9
bokdoong <102129660+BokDoong@users.noreply.github.com>
2024-12-20 10:41:33 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,4 +1,6 @@
--- 코드를 입력하세요
-SELECT COUNT(DISTINCT(name)) AS count
-FROM animal_ins
-WHERE name IS NOT NULL
+select count(*) as count
+from (
+    select distinct name
+    from animal_ins
+    where name is not null
+) as count_table
