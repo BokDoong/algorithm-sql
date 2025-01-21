e9af7aa8e32f3fcad2989daaeefbfec5860127e7
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-01-21 10:21:08 +0900

    Update 2758_로또.py

--- baekjoon-practice2/bruteforce/2758_로또.py
+++ baekjoon-practice2/bruteforce/2758_로또.py
@@ -1,26 +1,37 @@
-# DFS
-def solve(n, nums, depth, visited, start):
-  if depth == 6:
-    print(' '.join(result))
+import sys
+input = sys.stdin.readline
+
+
+# Methods
+def solve(k, nums, visited, idx, results, start):
+  if idx == 6:
+    print(' '.join(results))
     return
   
-  for i in range(start, n):
+  for i in range(start, k):
     if visited[i]:
       continue
     
     visited[i] = True
-    result.append(nums[i])
-    solve(n, nums, depth+1, visited, i)
-    result.pop()
+    results.append(str(nums[i]))
+    solve(k, nums, visited, idx+1, results, i)
+    results.pop()
     visited[i] = False
     
+
 # Main
-result = []
-visit = []
 while True:
-  nums = list(input().split())
-  if nums[0] == '0':
+  inputs = list(map(int,input().split()))
+
+  # 0이면 끝
+  if len(inputs) == 1 and inputs[0] == 0:
     break
-  visited = [False] * int(nums[0])
-  solve(int(nums[0]), nums[1:], 0, visited, 0)
+
+  # k, S
+  k = inputs[0]
+  nums = inputs[1:]
+
+  # 탐색
+  visited = [False] * k
+  solve(k, nums, visited, 0, [], 0)
   print()
