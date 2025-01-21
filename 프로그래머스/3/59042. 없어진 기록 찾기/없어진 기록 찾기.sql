dd621752a488b4d369e375feed4fcae5ca9060f2
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-01-21 09:45:18 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,5 @@
-select animal_outs.animal_id AS ANIMAL_ID, animal_outs.name AS NAME
-from animal_outs
-    left outer join animal_ins
-    on animal_outs.animal_id = animal_ins.animal_id
-where animal_ins.animal_id is null
+select ANIMAL_OUTS.ANIMAL_ID, ANIMAL_OUTS.NAME
+from ANIMAL_OUTS
+    left outer join ANIMAL_INS on ANIMAL_OUTS.ANIMAL_ID = ANIMAL_INS.ANIMAL_ID
+where ANIMAL_INS.ANIMAL_ID is null
+order by ANIMAL_INS.ANIMAL_ID
