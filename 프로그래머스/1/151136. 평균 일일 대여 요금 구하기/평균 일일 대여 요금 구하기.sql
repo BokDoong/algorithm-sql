3428fd4914ef3c0a2a1c5932d0a799505d159a77
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-02-05 11:41:44 +0900

    [level 1] Title: 평균 일일 대여 요금 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/151136. 평균 일일 대여 요금 구하기/평균 일일 대여 요금 구하기.sql
+++ 프로그래머스/1/151136. 평균 일일 대여 요금 구하기/평균 일일 대여 요금 구하기.sql
@@ -1,6 +1,3 @@
--- 코드를 입력하세요
-# 전체 조회
-# SELECT * FROM CAR_RENTAL_COMPANY_CAR
-SELECT ROUND(AVG(daily_fee)) AS AVERAGE_FEE
-    FROM car_rental_company_car
-    WHERE car_type = 'SUV'
+select round(avg(DAILY_FEE),0) as AVERAGE_FEE
+from CAR_RENTAL_COMPANY_CAR
+where CAR_TYPE = 'SUV'
