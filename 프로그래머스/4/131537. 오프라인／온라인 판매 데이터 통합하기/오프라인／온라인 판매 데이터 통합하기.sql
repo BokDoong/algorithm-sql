1f7e75ced754055209e5074d5da3b584de58742d
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-02-06 11:09:59 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,11 +1,12 @@
-select date_format(SALES_DATE, '%Y-%m-%d') as SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
-from ONLINE_SALE
-where date(SALES_DATE) >= '2022-03-01' and date(SALES_DATE) <= '2022-03-31'
+with ALL_DATA as (
+    select SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+    from ONLINE_SALE
+    union all
+    select SALES_DATE, PRODUCT_ID, null, SALES_AMOUNT
+    from OFFLINE_SALE
+)
 
-union
-
-select date_format(SALES_DATE, '%Y-%m-%d') as SALES_DATE, PRODUCT_ID, NULL as USER_ID, SALES_AMOUNT
-from OFFLINE_SALE
-where date(SALES_DATE) >= '2022-03-01' and date(SALES_DATE) <= '2022-03-31'
-
-order by SALES_DATE, PRODUCT_ID, USER_ID
+select date_format(SALES_DATE,'%Y-%m-%d') as SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+from ALL_DATA
+where SALES_DATE like '2022-03%'
+order by SALES_DATE asc, PRODUCT_ID asc, USER_ID asc
