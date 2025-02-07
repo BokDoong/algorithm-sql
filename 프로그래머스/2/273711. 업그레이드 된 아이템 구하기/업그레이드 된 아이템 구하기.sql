a1bcb6ed61c6d52859b28e8f7df9ee41cb05b387
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-02-07 13:24:58 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,6 +1,11 @@
-select item_info.item_id, item_info.item_name, item_info.rarity
-from item_info
-    inner join item_tree
-    on item_info.item_id = item_tree.item_id
-where item_tree.parent_item_id in (select item_id from item_info where rarity = 'RARE')
-order by item_info.item_id desc
+with RARE_ITEMS as (
+    select *
+    from ITEM_INFO
+    where RARITY = 'RARE'
+)
+
+select ITEM_TREE.ITEM_ID, ITEM_NAME, RARITY
+from ITEM_TREE
+    inner join ITEM_INFO on ITEM_TREE.ITEM_ID = ITEM_INFO.ITEM_ID
+where PARENT_ITEM_ID in (select ITEM_ID from RARE_ITEMS)
+order by ITEM_TREE.ITEM_ID desc
