8ccef88e76c6e5a55ced01479355e1d6218388ee
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-02-26 10:21:18 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,12 +1,28 @@
-select
-    case
-        when SKILL_CODE & (select sum(CODE) from SKILLCODES where CATEGORY like 'Front%') > 0 and SKILL_CODE & (select CODE from SKILLCODES where NAME = 'Python') > 0 then 'A'
-        when SKILL_CODE & (select CODE from SKILLCODES where NAME = 'C#') > 0 then 'B'
-        when SKILL_CODE & (select sum(CODE) from SKILLCODES where CATEGORY like 'Front%') > 0 then 'C'
-        else null
-    end as GRADE,
-    ID, EMAIL
+with 프론트엔드_기술 as (
+    select sum(CODE) as CODE
+    from SKILLCODES
+        where CATEGORY = 'Front End'
+), 파이썬 as (
+    select CODE
+    from SKILLCODES
+        where NAME = 'Python'
+), 씨 as (
+    select CODE
+    from SKILLCODES
+        where NAME = 'C#'
+), 등급표 as (
+    select 
+        case
+            when SKILL_CODE & (select CODE from 프론트엔드_기술) > 0 
+                and SKILL_CODE & (select CODE from 파이썬) > 0 then 'A'
+            when SKILL_CODE & (select CODE from 씨) > 0 then 'B'
+            when SKILL_CODE & (select CODE from 프론트엔드_기술) > 0 then 'C'
+        end as GRADE, ID
+    from DEVELOPERS
+)
+
+select GRADE, DEVELOPERS.ID, EMAIL
 from DEVELOPERS
-group by GRADE, ID, EMAIL
-having GRADE is not null
+    inner join 등급표 on DEVELOPERS.ID = 등급표.ID
+where GRADE is not null
 order by GRADE, ID
