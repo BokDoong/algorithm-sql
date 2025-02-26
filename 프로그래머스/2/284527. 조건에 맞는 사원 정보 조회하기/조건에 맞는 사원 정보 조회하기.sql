0628ef365e3cc77a9622ecc13abd4e90c1facec0
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-02-26 11:56:23 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,6 +1,12 @@
-select sum(SCORE) as SCORE, HR_EMPLOYEES.EMP_NO, EMP_NAME, POSITION, EMAIL
+with 최고_점수 as (
+    select EMP_NO, YEAR, sum(SCORE) as SCORE
+    from HR_GRADE
+    group by EMP_NO, YEAR
+        having YEAR = 2022
+    order by SCORE desc
+    limit 1
+)
+
+select 최고_점수.SCORE, HR_EMPLOYEES.EMP_NO, EMP_NAME, POSITION, EMAIL
 from HR_EMPLOYEES
-inner join HR_GRADE on HR_EMPLOYEES.emp_no = HR_GRADE.emp_no
-group by HR_EMPLOYEES.EMP_NO
-order by SCORE DESC
-LIMIT 1
+inner join 최고_점수 on HR_EMPLOYEES.EMP_NO = 최고_점수.EMP_NO
