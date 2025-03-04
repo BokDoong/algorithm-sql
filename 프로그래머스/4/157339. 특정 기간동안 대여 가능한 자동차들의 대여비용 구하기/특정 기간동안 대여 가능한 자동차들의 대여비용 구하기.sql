f3326442b36ff08a9e28a82201fadddc5a56d409
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-03-04 14:59:34 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,27 +1,23 @@
-with RENTAL_BANNED_CARS as (
-    select *
+with 11월_대여_불가능_자동차 as (
+    select distinct CAR_ID
     from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-        where '2022-11-01' between START_DATE and END_DATE or '2022-11-30' between START_DATE and END_DATE
-), THIRTY_DAYS_DISCOUNT_PLAN as (
-    select *
+    where END_DATE > '2022-11-01' AND START_DATE < '2022-12-01'
+), 자동차별_30일_할인율 as (
+    select CAR_TYPE, DISCOUNT_RATE/100 as DISCOUNT_RATE
     from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
     where DURATION_TYPE = '30일 이상'
-), CALCULATED_CARS as (
-    select CAR_ID, CAR_RENTAL_COMPANY_CAR.CAR_TYPE, round(DAILY_FEE * 30 * (100-DISCOUNT_RATE) * 0.01) as FEE
+), 자동차별_가격표 as (
+    select CAR_ID, CAR_RENTAL_COMPANY_CAR.CAR_TYPE, round(DAILY_FEE*30*(1.0 - DISCOUNT_RATE),0) as FEE
     from CAR_RENTAL_COMPANY_CAR
-        join THIRTY_DAYS_DISCOUNT_PLAN on CAR_RENTAL_COMPANY_CAR.CAR_TYPE = THIRTY_DAYS_DISCOUNT_PLAN.CAR_TYPE
-        where CAR_ID not in (
-            select CAR_ID
-            from RENTAL_BANNED_CARS
-        )
-    order by FEE desc, CAR_RENTAL_COMPANY_CAR.CAR_TYPE asc, CAR_ID desc
+    inner join 자동차별_30일_할인율
+        on CAR_RENTAL_COMPANY_CAR.CAR_TYPE = 자동차별_30일_할인율.CAR_TYPE
+    where CAR_ID not in (
+        select CAR_ID
+        from 11월_대여_불가능_자동차
+    ) and (CAR_RENTAL_COMPANY_CAR.CAR_TYPE = '세단' or CAR_RENTAL_COMPANY_CAR.CAR_TYPE = 'SUV')
 )
 
- # and (
- #        CAR_RENTAL_COMPANY_CAR.CAR_TYPE = '세단' or CAR_RENTAL_COMPANY_CAR.CAR_TYPE = 'SUV'
- #    ) and (FEE >= 500000 and FEE < 2000000)
-
 select *
-from CALCULATED_CARS
-    where (CAR_TYPE = '세단' or CAR_TYPE = 'SUV') and (FEE >= 500000 and FEE < 2000000)
+from 자동차별_가격표
+where FEE >= 500000 and FEE < 2000000
 order by FEE desc, CAR_TYPE asc, CAR_ID desc
