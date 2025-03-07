3c291606b720b09de1dbfe7caa593e26c307a347
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-03-07 17:02:56 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,22 +1,23 @@
-WITH value AS (
-    SELECT car.daily_fee, car.car_type, his.history_id,
-           DATEDIFF(end_date, start_date) + 1 AS period,
-    CASE 
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 90 THEN '90일 이상'
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 30 THEN '30일 이상'
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 7 THEN '7일 이상'
-      ELSE 'NONE' END AS duration_type
-FROM car_rental_company_rental_history AS his
-INNER JOIN car_rental_company_car AS car ON car.car_id = his.car_id
-WHERE car.car_type = '트럭')   
+set @weekDiscount := (select (100-DISCOUNT_RATE)/100 from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+    where CAR_TYPE = '트럭' and DURATION_TYPE = '7일 이상'),
+@monthDiscount := (select (100-DISCOUNT_RATE)/100 from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+    where CAR_TYPE = '트럭' and DURATION_TYPE = '30일 이상'),
+@threeMonthsDiscount := (select (100-DISCOUNT_RATE)/100 from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+    where CAR_TYPE = '트럭' and DURATION_TYPE = '90일 이상');
 
+with 트럭_대여시간 as (
+    select HISTORY_ID, CAR_RENTAL_COMPANY_RENTAL_HISTORY.CAR_ID, datediff(END_DATE, START_DATE) + 1 as DURATION, DAILY_FEE
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+        inner join CAR_RENTAL_COMPANY_CAR on CAR_RENTAL_COMPANY_RENTAL_HISTORY.CAR_ID = CAR_RENTAL_COMPANY_CAR.CAR_ID
+    where CAR_TYPE = '트럭'
+)
 
-
-SELECT value.history_id, 
-    ROUND(value.daily_fee * value.period * 
-          (100 - IFNULL(plan.discount_rate,0)) / 100) AS FEE
-FROM value
-LEFT JOIN car_rental_company_discount_plan AS plan 
-    ON plan.duration_type = value.duration_type 
-    AND plan.car_type = value.car_type
-ORDER BY 2 DESC, 1 DESC
+select HISTORY_ID,
+    case
+        when DURATION >= 90 then round(DURATION*DAILY_FEE*@threeMonthsDiscount,0)
+        when DURATION >= 30 then round(DURATION*DAILY_FEE*@monthDiscount, 0)
+        when DURATION >= 7 then round(DURATION*DAILY_FEE*@weekDiscount, 0)
+        else DURATION*DAILY_FEE
+    end as FEE
+from 트럭_대여시간
+order by FEE desc, HISTORY_ID desc
