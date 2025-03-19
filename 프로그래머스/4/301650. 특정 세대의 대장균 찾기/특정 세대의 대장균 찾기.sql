cbb02f1ca1a726b5884e6324bdb8aae991fe3712
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-03-19 20:09:53 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -13,8 +13,8 @@ with FIRST_GEN as (
 
 select ID
 from ECOLI_DATA
-where PARENT_ID in  (
-    select ID
-    from SECOND_GEN
-)
-order by ID asc
+where PARENT_ID in (
+        select ID
+        from SECOND_GEN
+    )
+order by ID
