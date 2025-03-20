d27405c608d25da4c2d4c9004f61752ecfd7fdec
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-03-20 18:07:35 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,12 +1,12 @@
-with MAX_LENGTH_INFO as (
-    select FISH_TYPE, max(LENGTH) as LENGTH
+with 종류별_큰_길이 as (
+    select FISH_TYPE, max(LENGTH) as MAX_LENGTH
     from FISH_INFO
     group by FISH_TYPE
 )
 
-select ID, FISH_NAME, FISH_INFO.LENGTH
+select ID, FISH_NAME, LENGTH
 from FISH_INFO
+    inner join 종류별_큰_길이 on FISH_INFO.FISH_TYPE = 종류별_큰_길이.FISH_TYPE
     inner join FISH_NAME_INFO on FISH_INFO.FISH_TYPE = FISH_NAME_INFO.FISH_TYPE
-    inner join MAX_LENGTH_INFO on FISH_INFO.FISH_TYPE = MAX_LENGTH_INFO.FISH_TYPE
-where FISH_INFO.LENGTH = MAX_LENGTH_INFO.LENGTH
+where LENGTH = MAX_LENGTH
 order by ID
