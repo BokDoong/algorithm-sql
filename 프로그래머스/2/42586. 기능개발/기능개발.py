c578e0bc67bb7fb613b18339fddb70165c281834
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-03-24 14:06:22 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,21 +2,33 @@ from collections import deque
 import math
 
 def solution(progresses, speeds):
+    # 정보 : (태스크 인덱스, 날짜)
+    info = deque([])
+    for i in range(len(progresses)):
+        info.append((progresses[i], speeds[i]))
+    
+    # 작업
     answer = []
-    
-    # 각 작업마다 남은 일수 계산
-    days = [math.ceil((100 - progresses[i]) / speeds[i]) for i in range(len(progresses))]
-    
-    # 순서대로 작업을 처리
-    now = days[0]
-    cnt = 1
-    for i in range(1, len(days)):
-        if days[i] <= now:
-            cnt += 1
-        else:
-            answer.append(cnt)
-            now = days[i]
-            cnt = 1
-    answer.append(cnt)  # 마지막 배포
-
+    while info:
+        # 맨 앞에 있는애 얼마나 걸리는지 계산
+        progress, speed = info.popleft()
+        days = math.ceil((100-progress)/speed)
+        
+        # 전체 카운팅
+        tmp = deque([])
+        while info:
+            p, s = info.popleft()
+            tmp.append((p+s*days, s))
+        info = tmp
+        
+        # popleft -> 100 넘으면 빼기
+        tmp = 1
+        while info:
+            progress, speed = info[0]
+            if progress >= 100:
+                info.popleft()
+                tmp += 1
+            else:
+                break
+        answer.append(tmp)
     return answer
