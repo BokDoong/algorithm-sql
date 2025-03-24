1523c8025b3c5557c40713b687c59f92e13d1370
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-03-24 14:15:42 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,16 +1,13 @@
-with 1월_도서_판매량 as (
-    select BOOK_ID, sum(SALES) as SALES
+with 1월_도서별_매출액 as (
+    select BOOK_SALES.BOOK_ID, CATEGORY, sum(SALES)*PRICE as TOTAL_SALES, AUTHOR_ID
     from BOOK_SALES
-        where SALES_DATE like '2022-01%'
-    group by BOOK_ID
-), 1월_도서_판매액 as (
-    select 1월_도서_판매량.BOOK_ID, SALES*BOOK.PRICE as TOTAL_PRICE, AUTHOR_ID, CATEGORY
-    from 1월_도서_판매량
-        inner join BOOK on 1월_도서_판매량.BOOK_ID = BOOK.BOOK_ID
+        inner join BOOK on BOOK_SALES.BOOK_ID = BOOK.BOOK_ID
+    where year(SALES_DATE) = 2022 and month(SALES_DATE) = 1
+    group by BOOK_SALES.BOOK_ID
 )
 
-select AUTHOR.AUTHOR_ID, AUTHOR_NAME, CATEGORY, sum(TOTAL_PRICE) as TOTAL_PRICE
-from 1월_도서_판매액
-    inner join AUTHOR on 1월_도서_판매액.AUTHOR_ID = AUTHOR.AUTHOR_ID
-group by AUTHOR_ID, AUTHOR_NAME, CATEGORY
-order by AUTHOR_ID asc, CATEGORY desc
+select AUTHOR.AUTHOR_ID, AUTHOR_NAME, CATEGORY, sum(TOTAL_SALES) as TOTAL_SALES
+from 1월_도서별_매출액
+    inner join AUTHOR on AUTHOR.AUTHOR_ID = 1월_도서별_매출액.AUTHOR_ID
+group by AUTHOR.AUTHOR_ID, AUTHOR_NAME, CATEGORY
+order by AUTHOR.AUTHOR_ID asc, CATEGORY desc
