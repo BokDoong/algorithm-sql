7a92e6ad01e6e5645c83e74e622aba760afae3bc
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-03-29 09:45:12 +0900

    [level 2] Title: 더 맵게, Time: 1917.70 ms, Memory: 50.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -2,15 +2,17 @@ import copy
 import heapq
 
 def solution(scoville, K):
+    # 정렬
     heapq.heapify(scoville)
-    mixCount = 0
-    
-    while len(scoville) >= 2 and scoville[0] < K:
+    tmp = 0
+    while scoville[0] < K and len(scoville)>=2:
         min1, min2 = heapq.heappop(scoville), heapq.heappop(scoville)
-        heapq.heappush(scoville, min1+min2*2)
-        mixCount += 1
+        heapq.heappush(scoville, min1+2*min2)
+        tmp+=1
+        
     
+    # 최소 값이 7보다 큰지 확인
     if scoville[0] >= K:
-        return mixCount
+        return tmp
     else:
         return -1
