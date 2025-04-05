50868859f385ae5e89b23cfa41a36b40b8cfa2ad
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-04-05 09:18:01 +0900

    Update 1260_DFS와 BFS.py

--- baekjoon-practice2/graph&bfs/1260_DFS와 BFS.py
+++ baekjoon-practice2/graph&bfs/1260_DFS와 BFS.py
@@ -1,59 +1,49 @@
 from collections import deque
-
-# Input
-N, M, V = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-for _ in range(M):
-  v1, v2 = map(int, input().split())
-  graph[v1].append(v2)
-  graph[v2].append(v1)
-  graph[v1].sort()
-  graph[v2].sort()
+import sys
+input = sys.stdin.readline
 
 # DFS
-visited1 = [0] * (N+1)
-vertexes1 = []
-def dfs(now, depth):
-  global N, visited1, vertexes1
-
-  # 탐색
-  for v in graph[now]:
-    if visited1[v]:
+def DFS(node):
+  global visited, graph, answer
+  for connected in graph[node]:
+    if not visited[connected]:
+      visited[connected] = True
+      answer.append(connected)
+      DFS(connected)
+  
+# BFS
+def BFS(start):
+  global visited, graph, answer
+  queue = deque([start])
+  while queue:
+    node = queue.popleft()
+    if visited[node]:
       continue
-    
-    vertexes1.append(v)
-    visited1[v] = True
-    dfs(v, depth+1)
+    answer.append(node)
+    visited[node] = True
+    for connected in graph[node]:
+      queue.append(connected)
 
+# 정점, 간선, 시작
+nodes, vertex, start = map(int,input().split())
+# 그래프
+graph = [[] for _ in range(nodes+1)]
+for _ in range(vertex):
+  node1, node2 = map(int,input().split())
+  graph[node1].append(node2)
+  graph[node2].append(node1)
+for node in range(1, nodes+1):
+  graph[node].sort()
+  
+# DFS
+visited = [0]*(nodes+1)
+visited[start] = True
+answer = [start]
+DFS(start)
+print(*answer)
 
 # BFS
-visited2 = [0] * (N+1)
-vertexes2 = []
-def bfs(now, depth):
-  global visited2, vertexes2, V
-  
-  queue = deque([V])
-  visited2[V] = True
-  while queue:
-    # 방문
-    v = queue.popleft()
-    vertexes2.append(v)
-    
-    # 큐에 넣기
-    for i in graph[v]:
-      if not visited2[i]:
-        queue.append(i)
-        visited2[i] = True
-
-
-# Main
-visited1[V] = True
-vertexes1.append(V)
-dfs(V, 1)
-for v in vertexes1:
-  print(v, end = ' ')
-
-print()
-bfs(V, 1)
-for v in vertexes2:
-  print(v, end = ' ')
+visited = [0]*(nodes+1)
+answer = []
+BFS(start)
+print(*answer)
