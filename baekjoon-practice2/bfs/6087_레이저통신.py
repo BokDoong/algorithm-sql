68048424fd55e0d463b9261015cdb8f3938ab70f
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-04-22 23:36:32 +0900

    Add files via upload

--- baekjoon-practice2/bfs/6087_레이저통신.py
+++ baekjoon-practice2/bfs/6087_레이저통신.py
@@ -1,39 +1,76 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-def bfs(x,y):
-  queue = deque([(x,y)])
-  visited[x][y] = 0
-  while queue:
-    x, y = queue.popleft()
-    for i in range(4):
-      nx,ny = x+dx[i], y+dy[i]
-      while True:
-        if not (0 <= nx < n and 0 <= ny < m):
-          break
-        if board[nx][ny] == '*':
-          break
-        if visited[nx][ny] < visited[x][y] + 1:
-          break
-        queue.append((nx,ny))
-        visited[nx][ny] = visited[x][y] + 1
-        nx = nx + dx[i]
-        ny = ny + dy[i]
+# 안 벗어나는지 검사
+def canMove(x, y):
+  global board, W, H
+  if x < 0 or y < 0 or x >= H or y >= W or (x,y) in walls:
+    return False
+  return True
 
+# 끝인지 검사
+def isDestination(x, y):
+  global destX, destY
+  return x == destX and y == destY
 
-m, n = map(int,input().split())
-board = [input() for _ in range(n)]
+# 옆방향 추출
+def extractReflectedVector(v):
+  if v == 0 or v == 1:
+    return [2, 3]
+  else:
+    return [0, 1]
 
-dx = (0, 1, 0, -1)
-dy = (1, 0, -1, 0)
+# C 좌표, 벽
+c = []
+walls = set()
+# W, H, 벽/도착점
+W, H = map(int, input().split())
+for h in range(H):
+  tmp = list(input().rstrip())
+  for w in range(W):
+    if tmp[w] == 'C':
+      c.append((h,w))
+    elif tmp[w] == '*':
+      walls.add((h,w))
 
-C = []
-for i in range(n):
-  for j in range(m):
-    if board[i][j] == 'C':
-      C.append((i,j))
+# 시작점, 도착점
+startX, startY = c[0][0], c[0][1]
+destX, destY = c[1][0], c[1][1]
+
+# 큐, 상하좌우 : 0/1/2/3
+queue = deque([])
+queue.append((startX, startY, 4, 0))
+dx, dy = [1,-1,0,0], [0,0,-1,1]
+
+# 보드
+board = [[False]*W for _ in range(H)]
+board[startX][startY] = 1
   
-(sx, sy), (ex, ey) = C
-visited = [[float("inf")] * m for _ in range(n)]
-bfs(sx,sy)
-
-print(visited[ex][ey] - 1)
+# BFS
+answer = 0 
+while queue:
+  nowX, nowY, v, mirror = queue.popleft()    # 좌표, 이동방향, 거울
+  # 끝인지 검사
+  if isDestination(nowX, nowY):
+    answer = mirror
+    break
+  # 레이저
+  for i in range(4):
+    # 다음방향, 다른 방향이면 +1
+    nextX, nextY = nowX+dx[i], nowY+dy[i]
+    tmpMirror = mirror
+    if i != v:
+      tmpMirror += 1
+    # 레이저
+    while True:
+      if canMove(nextX, nextY):
+        if not board[nextX][nextY]:
+          board[nextX][nextY] = tmpMirror
+          queue.append((nextX, nextY, i, tmpMirror))
+        nextX, nextY = nextX+dx[i], nextY+dy[i]
+      else:
+        break
+              
+# Output
+print(board[destX][destY]-1)
