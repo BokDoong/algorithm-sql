9dd9081a1a5110044b26334d5b9a96a466d34e15
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-04-27 01:02:35 +0900

    [level 3] Title: 섬 연결하기, Time: 0.05 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,30 +1,28 @@
 def solution(n, costs):
-    # 비용순 정렬
+    # 비용 정렬
     costs.sort(key = lambda x : x[2])
-    # 비용, 깊이, 부모테이블
-    answer = 0
-    depth = 0
+    # 부모, 비용(답), 깊이
     parents = [i for i in range(n)]
+    answer = 0
+    depth = 1
     
-    # 조상 찾기
+    # 조상 노드 찾기
     def find(node):
         if parents[node] == node:
             return node
         return find(parents[node])
-    
     # 합치기
     def union(nodeA, nodeB):
-        parentA = find(nodeA)
-        parentB = find(nodeB)
+        parentA, parentB = find(nodeA), find(nodeB)
         parents[parentB] = parentA
-        
+    
     # 크루스칼
-    while depth < n-1:
+    while depth < n:
         nodeA, nodeB, cost = costs.pop(0)
-        if find(nodeA) != find(nodeB):
+        parentA, parentB = find(nodeA), find(nodeB)
+        if parentA != parentB:
             union(nodeA, nodeB)
             answer += cost
             depth += 1
         
-    
     return answer
