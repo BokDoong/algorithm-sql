0e9d7d41578f97fbc3257acbc9d92aac8d8ed558
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-05-07 10:11:17 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,23 +1,17 @@
-set @weekDiscount := (select (100-DISCOUNT_RATE)/100 from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-    where CAR_TYPE = '트럭' and DURATION_TYPE = '7일 이상'),
-@monthDiscount := (select (100-DISCOUNT_RATE)/100 from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-    where CAR_TYPE = '트럭' and DURATION_TYPE = '30일 이상'),
-@threeMonthsDiscount := (select (100-DISCOUNT_RATE)/100 from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-    where CAR_TYPE = '트럭' and DURATION_TYPE = '90일 이상');
-
-with 트럭_대여시간 as (
-    select HISTORY_ID, CAR_RENTAL_COMPANY_RENTAL_HISTORY.CAR_ID, datediff(END_DATE, START_DATE) + 1 as DURATION, DAILY_FEE
+with 트럭_대여정보 as (
+    select HISTORY_ID, datediff(END_DATE, START_DATE)+1 as RENT_DATE, (datediff(END_DATE, START_DATE)+1)*DAILY_FEE as FEE
     from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-        inner join CAR_RENTAL_COMPANY_CAR on CAR_RENTAL_COMPANY_RENTAL_HISTORY.CAR_ID = CAR_RENTAL_COMPANY_CAR.CAR_ID
-    where CAR_TYPE = '트럭'
+    inner join (
+        select CAR_ID, DAILY_FEE from CAR_RENTAL_COMPANY_CAR where CAR_TYPE = '트럭'
+    ) as 트럭_정보 on CAR_RENTAL_COMPANY_RENTAL_HISTORY.CAR_ID = 트럭_정보.CAR_ID
 )
 
 select HISTORY_ID,
     case
-        when DURATION >= 90 then round(DURATION*DAILY_FEE*@threeMonthsDiscount,0)
-        when DURATION >= 30 then round(DURATION*DAILY_FEE*@monthDiscount, 0)
-        when DURATION >= 7 then round(DURATION*DAILY_FEE*@weekDiscount, 0)
-        else DURATION*DAILY_FEE
+        when RENT_DATE >= 90 then round(FEE*(1-0.01*(select DISCOUNT_RATE from CAR_RENTAL_COMPANY_DISCOUNT_PLAN where CAR_TYPE = '트럭' and DURATION_TYPE = '90일 이상')), 0)
+        when RENT_DATE >= 30 then round(FEE*(1-0.01*(select DISCOUNT_RATE from CAR_RENTAL_COMPANY_DISCOUNT_PLAN where CAR_TYPE = '트럭' and DURATION_TYPE = '30일 이상')), 0)
+        when RENT_DATE >= 7 then round(FEE*(1-0.01*(select DISCOUNT_RATE from CAR_RENTAL_COMPANY_DISCOUNT_PLAN where CAR_TYPE = '트럭' and DURATION_TYPE = '7일 이상')), 0)
+        else FEE
     end as FEE
-from 트럭_대여시간
+from 트럭_대여정보
 order by FEE desc, HISTORY_ID desc
