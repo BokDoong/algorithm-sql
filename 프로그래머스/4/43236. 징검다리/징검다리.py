0f11da8ef7cf7af4ec42727fae6b834ac095db08
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-06-05 17:48:32 +0900

    [level 4] Title: 징검다리, Time: 122.32 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,28 +1,29 @@
 def solution(distance, rocks, n):
-    answer = 0
-    
+    # 바위 정렬 + 끝점 넣기
+    rocks.sort()
     rocks.append(distance)
-    rocks = sorted(rocks)
     
-    left, right = 0, distance
-    while left <= right:
-        mid = (left+right) // 2
-        minDist = float('inf')
+    # 탐색
+    start, end = 0, rocks[-1]
+    while start <= end :
+        mid = (start+end) // 2
+        cntRemove = 0   # 제거한 바위 수
         current = 0
-        removeCnt = 0
         
+        # 제거할 바위 수 세기
         for rock in rocks:
-            diff = rock - current
-            if diff < mid:
-                removeCnt += 1
+            dist = rock - current
+            if dist < mid:
+                cntRemove += 1
+                if cntRemove > n:
+                    break
             else:
                 current = rock
-                minDist = min(minDist, diff)
                 
-        if removeCnt > n:
-            right = mid - 1
+        if cntRemove > n:
+            end = mid-1
         else:
-            answer = minDist
-            left = mid + 1
+            answer = mid
+            start = mid+1
         
     return answer
