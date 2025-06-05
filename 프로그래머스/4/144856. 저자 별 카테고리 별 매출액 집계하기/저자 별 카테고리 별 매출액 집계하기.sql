dbb28bd7607260d600b3c55424f96cd0dfb08d2c
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-06-05 16:24:24 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,13 +1,10 @@
-with 1월_도서별_매출액 as (
-    select BOOK_SALES.BOOK_ID, CATEGORY, sum(SALES)*PRICE as TOTAL_SALES, AUTHOR_ID
-    from BOOK_SALES
-        inner join BOOK on BOOK_SALES.BOOK_ID = BOOK.BOOK_ID
-    where year(SALES_DATE) = 2022 and month(SALES_DATE) = 1
-    group by BOOK_SALES.BOOK_ID
-)
-
-select AUTHOR.AUTHOR_ID, AUTHOR_NAME, CATEGORY, sum(TOTAL_SALES) as TOTAL_SALES
-from 1월_도서별_매출액
-    inner join AUTHOR on AUTHOR.AUTHOR_ID = 1월_도서별_매출액.AUTHOR_ID
-group by AUTHOR.AUTHOR_ID, AUTHOR_NAME, CATEGORY
-order by AUTHOR.AUTHOR_ID asc, CATEGORY desc
+select BOOK.AUTHOR_ID, AUTHOR_NAME, CATEGORY, sum(SALES*PRICE) as TOTAL_SALES
+from BOOK
+    inner join (
+        select BOOK_ID, SALES, SALES_DATE
+        from BOOK_SALES
+        where year(SALES_DATE) = 2022 and month(SALES_DATE) = 1
+    ) as JAN_BOOK on BOOK.BOOK_ID = JAN_BOOK.BOOK_ID
+    inner join AUTHOR on BOOK.AUTHOR_ID = AUTHOR.AUTHOR_ID
+group by BOOK.AUTHOR_ID, CATEGORY
+order by AUTHOR_ID, CATEGORY desc
