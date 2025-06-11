7564029ce85555bf84d5ce9fcd862a17cd51c948
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-06-12 08:31:31 +0900

    Update 1339_단어수학.py

--- baekjoon-practice2/bruteforce/1339_단어수학.py
+++ baekjoon-practice2/bruteforce/1339_단어수학.py
@@ -1,28 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-# 단어개수
+# Input
 N = int(input().rstrip())
-S = []
+numbers = []
 for _ in range(N):
-  S.append(input().rstrip())
+  numbers.append(input().rstrip())
+
+# 알파벳
+alphabets = [0] * 26
+for number in numbers:
+  for i in range(len(number)):
+    alphabets[ord(number[i])-65] += 10**(len(number)-1-i)
+
+# 정렬해서 출력
+alphabets.sort(reverse=True)
+answer, cnt = 0, 9
+for i in range(10):
+  answer += (alphabets[i]*cnt)
+  cnt -= 1
   
-# 단어별 값을 지정
-words = {}
-for s in S:
-  x = len(s)-1
-  for i in s:
-    if i in words:
-      words[i] += 10**x
-    else:
-      words[i] = 10**x
-    x-=1
-  
-# 딕셔너리 value만
-sortedWords = sorted(words.values(),reverse=True)
-result = 0
-num = 9
-for k in sortedWords:
-  result += k*num
-  num -= 1
-print(result)
+print(answer)
