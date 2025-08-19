3e54425081dc6e9d6351baaf491bbc8c8e580e7e
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-08-19 13:47:45 +0900

    Update 16197_두 동전.py

--- baekjoon-practice2/back_tracking/16197_두 동전.py
+++ baekjoon-practice2/back_tracking/16197_두 동전.py
@@ -1,73 +1,53 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 
-# 보드 세로, 가로
-N, M = map(int, input().split())
-
-# 보드
-board = [input().rstrip() for _ in range(N)]
-  
-# 결과값
-result = sys.maxsize
-
-# 이동 벡터
-dxs = [1, -1, 0, 0]
-dys = [0, 0, -1, 1]
-
-# 동전 첫 위치
-def getStartPos():
-  coinPos = []
-  for i in range(N):
-    for j in range(M):
-      if board[i][j] == 'o':
-        coinPos.append((i, j))
-  return coinPos
-
-# 범위 내에 있는지 확인
-def inRange(x, y):
-  return 0 <= x < N and 0 <= y < M
-
-# 갈 수 있는지 확인
-def canGo(x, y):
+# 범위 안인지 체크
+def canMove(x, y):
+  global N, M
   if 0 <= x < N and 0 <= y < M:
-    return not board[x][y] == '#'
-  return True
-  
-# 백트래킹
-def backTracking(depth, firstX, firstY, secondX, secondY):
-  global result
-  
-  # 둘다 떨어졌는지 확인
-  if not inRange(firstX, firstY) and not inRange(secondX, secondY):
-    return
-  # 둘 중 하나만 떨어졌는지 확인
-  elif not inRange(firstX, firstY) or not inRange(secondX, secondY):
-    result = min(result, depth)
-    return
-  
-  # 10번 이상 떨어지면 리턴
-  if depth == 10:
-    return
-  
-  # 이동
-  for dx, dy in zip(dxs, dys):
-    # 두 동전의 다음 이동 좌표
-    nextFirstX, nextFirstY, nextSecondX, nextSecondY = firstX+dx, firstY+dy, secondX+dx, secondY+dy
-    
-    if canGo(nextFirstX, nextFirstY) and canGo(nextSecondX, nextSecondY):
-      backTracking(depth+1, nextFirstX, nextFirstY, nextSecondX, nextSecondY)
-    elif canGo(nextSecondX, nextSecondY):
-      backTracking(depth+1, firstX, firstY, nextSecondX, nextSecondY)
-    elif canGo(nextFirstX, nextFirstY):
-      backTracking(depth+1, nextFirstX, nextFirstY, secondX, secondY)
+    return True
+  return False
 
-# Main
-firstCoinPos, secondCoinPos = getStartPos()
-backTracking(0, firstCoinPos[0], firstCoinPos[1], secondCoinPos[0], secondCoinPos[1])
+# 벽인지 체크
+def isWall(x, y):
+  global board
+  if board[x][y] == '#':
+    return True
+  return False
 
+# 입력
+N, M = map(int, input().split())
+board = [list(input().rstrip()) for _ in range(N)]
 
-# Output
-if result == sys.maxsize:
-  print(-1)
-else:
-  print(result)
+# 동전 찾기
+pos = []
+for n in range(N):
+  for m in range(M):
+    if board[n][m] == 'o':
+      pos.append(n)
+      pos.append(m)
+x1, y1, x2, y2 = pos[0], pos[1], pos[2], pos[3]
+
+# 굴리기
+dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+queue = deque([(x1, y1, x2, y2, 0)])
+while queue:
+  nowX1, nowY1, nowX2, nowY2, rollCount = queue.popleft()
+  if rollCount > 10:
+    print(-1)
+    exit()
+  for i in range(4):
+    nextX1, nextY1, nextX2, nextY2 = nowX1 + dx[i], nowY1 + dy[i], nowX2 + dx[i], nowY2 + dy[i]
+    if canMove(nextX1, nextY1) and canMove(nextX2, nextY2):
+      if isWall(nextX1, nextY1):
+        nextX1, nextY1 = nowX1, nowY1
+      if isWall(nextX2, nextY2):
+        nextX2, nextY2 = nowX2, nowY2
+      queue.append((nextX1, nextY1, nextX2, nextY2, rollCount+1))
+    elif canMove(nextX1, nextY1):
+      print(rollCount+1)
+      exit()
+    elif canMove(nextX2, nextY2):
+      print(rollCount+1)
+      exit()
