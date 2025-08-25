f7fa46a814250864beda4ac03ebd898766d2d666
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-08-25 09:03:38 +0900

    Create 1907. Count Salary Categories.sql

---
+++ 1907. Count Salary Categories.sql
@@ -0,0 +1,12 @@
+select 'Low Salary' as category, sum(income < 20000) as accounts_count
+from Accounts
+
+union
+
+select 'Average Salary' as category, sum(income >= 20000 and income < 50000) as accounts_count
+from Accounts
+
+union
+
+select 'High Salary' as category, sum(income >= 50000) as accounts_count
+from Accounts
