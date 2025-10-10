9b9c587cd705c39537f2a3b48f1544d6a5268f61
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-10-11 00:43:28 +0900

    [level 3] Title: 여행경로, Time: 359.35 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,18 +1,37 @@
+from collections import deque
+
 def solution(tickets):
     
-    answer = ['ICN']
-    visited = [False] * len(tickets)
-    tickets.sort(key = lambda x : (x[0], x[1]))
-    
-    def DFS(airport, path):
-        if len(path) == len(tickets) + 1:
-            answer.append(path)
+    # 그래프 구성
+    graph = {}
+    for start, end in tickets:
+        if start in graph:
+            graph[start].append((end))
+        else:
+            graph[start] = [end]
+            
+    # 그래프 정렬
+    for city in graph:
+        graph[city].sort()
+        graph[city] = deque(graph[city])
+            
+    # DFS
+    def DFS(start, path):
+        nonlocal result, tickets
+        
+        if len(path) == len(tickets)+1:
+            result.append(path)
             return
-        for idx, ticket in enumerate(tickets):
-            if ticket[0] == airport and not visited[idx]:
+        
+        for idx, cities in enumerate(tickets):
+            if cities[0] == start and not visited[idx]:
                 visited[idx] = True
-                DFS(ticket[1], path+[ticket[1]])
+                DFS(cities[1], path + [cities[1]])
                 visited[idx] = False
-                
-    DFS("ICN", ["ICN"])
-    return answer[1]
+    
+    result = []
+    visited = [False for _ in range(len(tickets))]
+    DFS('ICN', ['ICN'])
+    result.sort()
+    
+    return result[0]
