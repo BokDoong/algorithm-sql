4591b7c04aac31ff743fd1535f3d7cb9a55d1a6b
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-10-11 11:24:36 +0900

    [level 2] Title: 구명보트, Time: 14.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,19 +1,20 @@
 from collections import deque
 
 def solution(people, limit):
-    answer = 0
     
-    # 정렬
-    people.sort()
-    queue = deque(people)
+    result = 0
+    queue = deque(sorted(people, reverse=True))
     
-    # 구조
-    while queue:
-        # 가장 무거운 사람 빼기
-        heaviest = queue.pop()
-        # 가장 가벼운 사람과 함께 빠질 수 있다면 빠지기
-        if len(queue) > 0 and (heaviest + queue[0]) <= limit:
+    while len(queue) > 1:
+        if queue[0] + queue[-1] <= limit:
+            result += 1
             queue.popleft()
-        answer += 1
+            queue.pop()
+        else:
+            result += 1
+            queue.popleft()
+            
+    if queue:
+        result += 1
         
-    return answer
+    return result
