89c2f87d01db12fdcdfc41e5b56e345d519764dd
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-10-14 22:41:44 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,6 +1,7 @@
-with 사월별_등급 as (
-    select EMP_NO,
-        case
+with 평가등급_정보 as (
+    select
+        EMP_NO,
+        case 
             when avg(SCORE) >= 96 then 'S'
             when avg(SCORE) >= 90 then 'A'
             when avg(SCORE) >= 80 then 'B'
@@ -10,13 +11,13 @@ with 사월별_등급 as (
     group by EMP_NO
 )
 
-select HR_EMPLOYEES.EMP_NO, EMP_NAME, GRADE, 
+select HR_EMPLOYEES.EMP_NO, EMP_NAME, GRADE,
     case
         when GRADE = 'S' then SAL*0.2
         when GRADE = 'A' then SAL*0.15
         when GRADE = 'B' then SAL*0.1
-        when GRADE = 'C' then 0
+        else 0
     end as BONUS
 from HR_EMPLOYEES
-inner join 사월별_등급 on HR_EMPLOYEES.EMP_NO = 사월별_등급.EMP_NO
+inner join 평가등급_정보 on HR_EMPLOYEES.EMP_NO = 평가등급_정보.EMP_NO
 order by HR_EMPLOYEES.EMP_NO
