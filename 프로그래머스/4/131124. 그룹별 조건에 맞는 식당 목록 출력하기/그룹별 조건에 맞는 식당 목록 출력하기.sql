ab272335b6f17a3d75536ebc51392073dd532737
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-10-17 18:01:00 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,16 +1,14 @@
-select MEMBER_NAME, REVIEW_TEXT, date_format(REVIEW_DATE, '%Y-%m-%d') as REVIEW_DATE
-from MEMBER_PROFILE
-    inner join REST_REVIEW on MEMBER_PROFILE.MEMBER_ID = REST_REVIEW.MEMBER_ID
-where MEMBER_PROFILE.MEMBER_ID in (
-    select MEMBER_ID
+with MAX_REVIEW_MEMBER as (
+    select MEMBER_ID, count(REVIEW_ID) as REVIEW_COUNT
     from REST_REVIEW
     group by MEMBER_ID
-        having count(REVIEW_ID) = (
-            select max(REVIEW_IDS_CNT) 
-            from (
-                select MEMBER_ID, count(REVIEW_ID) as REVIEW_IDS_CNT
-                from REST_REVIEW
-                group by MEMBER_ID
-            ) as REST_REVIEW_CNTS)
+    order by REVIEW_COUNT desc
+    limit 1
 )
+
+select MEMBER_PROFILE.MEMBER_NAME, REVIEW_TEXT, date_format(REVIEW_DATE, '%Y-%m-%d') as REVIEW_DATE
+from REST_REVIEW
+inner join MEMBER_PROFILE
+    on REST_REVIEW.MEMBER_ID = MEMBER_PROFILE.MEMBER_ID
+where REST_REVIEW.MEMBER_ID = (select MEMBER_ID from MAX_REVIEW_MEMBER)
 order by REVIEW_DATE, REVIEW_TEXT
