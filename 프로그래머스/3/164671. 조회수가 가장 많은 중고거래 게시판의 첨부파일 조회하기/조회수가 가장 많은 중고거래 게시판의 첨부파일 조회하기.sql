6d58910142d4c9ff0c613741e411ece648e5d85f
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-10-22 22:10:30 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,13 +1,9 @@
-with 가장_조회수_높은_게시글_ID as (
-    select BOARD_ID
-    from USED_GOODS_BOARD
-    where VIEWS = (
-        select max(VIEWS)
-        from USED_GOODS_BOARD
-    )
-)
-
 select concat('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) as FILE_PATH
 from USED_GOODS_FILE
-where BOARD_ID = (select BOARD_ID from 가장_조회수_높은_게시글_ID)
+where BOARD_ID = (
+    select BOARD_ID
+    from USED_GOODS_BOARD
+    order by VIEWS desc
+    limit 1
+)
 order by FILE_ID desc
