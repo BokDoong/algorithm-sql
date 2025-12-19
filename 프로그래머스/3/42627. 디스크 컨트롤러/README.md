7240f7b925aad8a28465d37234c7f6e3158a3a68
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-12-19 22:47:15 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 7.12 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 95.4 MB, 시간: 25.45 ms
+메모리: 76.1 MB, 시간: 7.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 01일 21:33:51
+2025년 12월 19일 22:47:12
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -3,153 +3,82 @@ import java.io.*;
 
 class Solution {
     
-    public void debug() {
-        
-        for ( Integer t : jobMap.keySet() ) {
-            System.out.print(t + " -> ");
-            List<Job> jobList = jobMap.get(t);
-            for ( Job job : jobList ) {
-                System.out.print(job);
-            }
-            System.out.println();
-        }
-        
-    }
-    
-    HashMap<Integer, List<Job>> jobMap = new HashMap<>();
-    PriorityQueue<Job> heap = new PriorityQueue<>(1, new JobComparator());
-    List<Integer> returnTimes = new ArrayList<>();
-    
     class Job {
         
-        int idx;
+        int id;
         int requestTime;
         int takenTime;
         
-        public Job ( int idx, int requestTime, int takenTime ) {
-            this.idx = idx;
+        public Job(int id, int requestTime, int takenTime) {
+            this.id = id;
             this.requestTime = requestTime;
             this.takenTime = takenTime;
         }
         
-        public int calculateReturnTimes(int endTime) { 
-            return endTime - requestTime;
-        }
-        
         @Override
         public String toString() {
-            return "idx : " + idx + ", requestTime : " + requestTime + ", takenTime : " + takenTime;
+            return "id : " + id + ", requestTime : " + requestTime + ", takenTime : " + takenTime;
         }
         
     }
     
-    class JobComparator implements Comparator<Job> {
+    public int solve(int[][] jobs) {
         
-        @Override
-        public int compare(Job j1, Job j2) {
-            if ( j1.takenTime == j2.takenTime && j1.requestTime == j2.requestTime ) {
-                return j1.idx - j2.idx;
-            } else if ( j1.takenTime == j2.takenTime ) {
-                return j1.requestTime - j2.requestTime;
-            } else {
-                return j1.takenTime - j2.takenTime;
-            }
-        }
-        
-    }
-    
-    public void inputToMap(int[][] jobs) {
-        
-        for ( int i = 0 ; i < jobs.length ; i++ ) {
-            
-            Job job = new Job(i, jobs[i][0], jobs[i][1]);
-            
-            if (!jobMap.containsKey(jobs[i][0])) {
-                List<Job> jobList = new ArrayList<>();
-                jobList.add(job);
-                jobMap.put(jobs[i][0], jobList);
-            } else {
-                List<Job> jobList = jobMap.get(jobs[i][0]);
-                jobList.add(job);
-            }
-            
-        }
-        
-    }
-    
-    public void solve(int[][] jobs) {
-        
-        // 첫 요청시간 부터
-        int time = jobs[0][0];
-        
-        // 수행
-        while ( returnTimes.size() < jobs.length ) {
-            
-            // 현재 시간에서 수행할 작업 넣기
-            if ( jobMap.containsKey(time) ) {
-                List<Job> firstJobs = jobMap.get(time);
-                for ( Job job : firstJobs ) {
-                    heap.add(job);
-                }
-            }
-            
-            // 수행할 업무가 있다면 
-            if ( !heap.isEmpty() ) {
-                
-                Job todoJob = heap.poll();   
-                
-                // 작업이 끝나는 시간까지 들어오는 데이터힙에 추가
-                int endTime = time + todoJob.takenTime;
-                for ( int t = time+1 ; t < endTime ; t++ ) {
-                    if ( jobMap.containsKey(t) ) {
-                        List<Job> requestedJobs = jobMap.get(t);
-                        for ( Job job : requestedJobs ) {
-                            heap.add(job);
-                        }
-                    }
-                }
-                
-                // 반환 시간 계산
-                Integer returnTime = todoJob.calculateReturnTimes(endTime);
-                returnTimes.add(returnTime);
-                
-                // 다음 시간으로 
-                time = endTime;
-                
-            } else {
-                
-                // 해야할 일 없다면 시간 지나가기
-                time++;
-                
-            }
-            
-        }
-        
-    }
-    
-    public int calculateAverageReturnTimes() {
-        Integer result = 0;
-        for ( Integer returnTime : returnTimes ) {
-            result += returnTime;
-        }
-        return (int) result.intValue() / returnTimes.size();
-    }
-
-    
-    public int solution(int[][] jobs) {
-        
-        // 요청시간 기준으로 정렬
+        // 요청 시점 기준 정렬
         Arrays.sort(jobs, Comparator.comparingInt((int[] job) -> job[0]));
         
-        // 각 요청을 Map에 넣기
-        inputToMap(jobs);
+        // 최소힙 초기화
+        PriorityQueue<Job> queue = new PriorityQueue<>(
+            Comparator.comparingInt((Job job) -> job.takenTime)
+                    .thenComparingInt(job -> job.requestTime)
+                    .thenComparingInt(job -> job.id)
+        );
         
-        // 작업 수행
-        solve(jobs);
+        // 수행 개수, 총 반환시간, 큐에 넣은 윈도우
+        int done = 0;
+        int totalReturnTime = 0;
+        int window = 0;
+        int time = 0;
         
-        // 반환시간 평균 계산
-        int answer = calculateAverageReturnTimes();
-        return answer;
-        
+        // ㄱㄱ
+        while ( done < jobs.length ) {
+            
+            // 시간 보다 작거나 같은 애들 window부터 쭉 넣기
+            while ( true ) {
+                
+                // 마지막이라면 벗어나기
+                if ( window == jobs.length) {
+                    break;
+                }
+                
+                // 작업 넣기
+                int[] job = jobs[window];
+                if ( job[0] <= time ) {
+                    queue.add(new Job(window, job[0], job[1]));
+                    window += 1;
+                } else {
+                    break;
+                }
+                
+            }
+            
+            // 큐에 작업 남아있다면 수행 시간 만큼 점프 -> 반환시간, done 계산
+            if ( !queue.isEmpty() ) {
+                Job todo = queue.poll();
+                time += todo.takenTime;
+                done += 1;
+                totalReturnTime += (time - todo.requestTime);
+            } 
+            // 아니라면 1초 증가
+            else {
+                time += 1;
+            }
+            
+        }
+        return totalReturnTime / jobs.length;
+    }
+    
+    public int solution(int[][] jobs) {
+        return solve(jobs);
     }
 }
