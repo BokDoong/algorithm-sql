03611c49242c3ec7baa49f6c739b6bcc286fd8f3
bokdoong <102129660+BokDoong@users.noreply.github.com>
2026-01-02 09:17:49 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,10 +1,8 @@
-with 프론트엔드_기술스택 as (
-    select sum(CODE) as FRONT_CODE
-    from SKILLCODES
-    where CATEGORY like 'Front%'
-)
-
-select ID, EMAIL, FIRST_NAME, LAST_NAME
-from DEVELOPERS
-where SKILL_CODE & (select FRONT_CODE from 프론트엔드_기술스택) > 0
-order by ID
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS
+WHERE (
+    SELECT SUM(CODE) AS CODES
+    FROM SKILLCODES
+    WHERE CATEGORY LIKE 'Front%'
+) & SKILL_CODE > 0
+ORDER BY ID
