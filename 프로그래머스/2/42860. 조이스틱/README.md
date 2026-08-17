d9ce509a09f1ead67375d6b3f85a906eb374545d
bokdoong <102129660+BokDoong@users.noreply.github.com>
2026-08-17 16:10:17 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 82.8 MB, 시간: 0.07 ms
+메모리: 81 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 20일 08:17:55
+2026년 08월 17일 16:10:14
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,50 +1,33 @@
-import java.util.*;
-
 class Solution {
     public int solution(String name) {
-
-        // 각 자리마다 얼마나 이동해야하는지 정하기 (세로 이동)
-        int[] dist = new int[name.length()];
-        for (int i = 0; i < name.length(); i++) {
-            dist[i] = Math.min('Z' - name.charAt(i) + 1, name.charAt(i) - 'A');
-        }
-
-        // System.out.println(Arrays.toString(dist));
-
-        // 세로 이동 합 (A -> 각 알파벳)
         int answer = 0;
-        for (int i = 0; i < dist.length; i++) {
-            answer += dist[i];
+        int n = name.length();
+        
+        // 각 자리마다 이동해야 하는 갯수
+        for (char c : name.toCharArray()) {
+            answer += Math.min(c - 'A', 'Z' - c + 1);
         }
-
-        // 한 자리라면 끝 (가로 이동 없음)
-        if (name.length() == 1) {
-            return answer;
-        }
-
-        // 가로 이동 최소값 구하기
-        // 기본: 그냥 오른쪽으로 끝까지 가는 경우
-        int move = name.length() - 1;
-
-        for (int i = 0; i < name.length(); i++) {
-
-            // i 다음부터 연속된 A 구간 끝까지 스킵
+        
+        // 이동 방향 선정
+        // 1 : 오른쪽 쭉 이동
+        int move = n - 1;
+        
+        // 모든 꺽는 후보
+        for (int i = 0; i < n; i++) {
+            // A 연속으로 오는 지점 
             int next = i + 1;
-            while (next < name.length() && name.charAt(next) == 'A') {
+            while (next < n && name.charAt(next) == 'A') {
                 next++;
             }
-
-            // 케이스 1) 오른쪽으로 i까지 갔다가 되돌아오고(턴) -> 뒤쪽 처리
-            // 오른쪽 i + 되돌아 i + (끝에서 next까지)
-            int case1 = i * 2 + (name.length() - next);
-
-            // 케이스 2) 뒤쪽(왼쪽) 먼저 갔다가 -> 다시 오른쪽 처리
-            // (끝에서 next까지)*2 + i
-            int case2 = (name.length() - next) * 2 + i;
-
-            move = Math.min(move, Math.min(case1, case2));
+            
+            // 오른쪽 i까지 -> 돌아와서 -> 역순 next까지
+            move = Math.min(move, i + i + (n - next));
+            
+            // next로 역순 -> 다시 돌아와서 i까지
+            move = Math.min(move, (n - next) + (n - next) + i);
         }
-
-        return answer + move;
+        
+        answer += move;
+        return answer;
     }
 }
