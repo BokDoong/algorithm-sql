eb7001f9d0a37a2844adece65df6060cca618319
bokdoong <102129660+BokDoong@users.noreply.github.com>
2026-08-18 20:38:51 +0900

    [level 3] Title: 섬 연결하기, Time: 0.63 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -6,35 +6,39 @@ class Solution {
     
     public int solution(int n, int[][] costs) {
         
-        int answer = 0;
-        parent = new int[n];
-        
-        // 비용순 정렬
+        // 정렬
         Arrays.sort(costs, (cost1, cost2) -> cost1[2] - cost2[2]);
         
         // 초기화
-        init(n);
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
         
-        // 유니온 & 파인드 : 어차피 같은 그룹이면 간선이 안만들어짐
-        int edges = 0;
+        // union-find
+        int answer = 0;
+        int v = 0;
         for (int[] cost : costs) {
-            // 간선의 개수가 n-1이 될 때까지 순회
-            if (edges == n-1) break;
-            
-            // 같은 그룹인지 보고 > 아니라면 합치기, 비용 더하기
+            // 간선이 n-1개 될 때까지 합치기
+            if (v == n-1) break;
+            // 같은 그룹이면 합쳐지지 않음
             if (union(cost[0], cost[1])) {
+                v++;
                 answer += cost[2];
-                edges++;
             }
         }
         
         return answer;
     }
     
-    void init(int n) {
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+    boolean union(int nodeA, int nodeB) {
+        int rootA = findRoot(nodeA);
+        int rootB = findRoot(nodeB);
+        if (rootA != rootB) {
+            parent[rootA] = rootB; 
+            return true;
         }
+        return false;
     }
     
     int findRoot(int node) {
@@ -43,13 +47,4 @@ class Solution {
         return parent[node];
     }
     
-    boolean union(int nodeA, int nodeB) {
-        int rootA = findRoot(nodeA);
-        int rootB = findRoot(nodeB);
-        // 같은 그룹
-        if (rootA == rootB) return false;
-        // 합치기
-        parent[rootA] = rootB;
-        return true;
-    }
 }
