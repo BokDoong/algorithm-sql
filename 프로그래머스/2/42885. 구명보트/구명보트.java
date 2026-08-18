1d98b5c97d2cfe4d08b5471817bc106c5419f190
bokdoong <102129660+BokDoong@users.noreply.github.com>
2026-08-18 10:48:38 +0900

    [level 2] Title: 구명보트, Time: 10.82 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -7,15 +7,13 @@ class Solution {
         
         // 정렬
         Arrays.sort(people);
-        int light = 0;
-        int heavy = people.length - 1;
         
+        // 무거운 사람 태우고 > 가벼운 사람 태울 수 있으면 태우기
+        int light = 0; int heavy = people.length - 1;
         while (light <= heavy) {
-            // 가벼운 light 태울 수 있으면 태우기
-            if (people[light] + people[heavy] <= limit) {
+            if (people[light] <= limit - people[heavy]) {
                 light++;
             }
-            // 무거운 사람은 무조건 탑승
             heavy--;
             answer++;
         }
