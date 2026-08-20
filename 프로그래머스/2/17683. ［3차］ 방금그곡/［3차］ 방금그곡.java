4eb95c2c830f87caac8bf66941e2299d7fa9a6c7
bokdoong <102129660+BokDoong@users.noreply.github.com>
2026-08-20 10:17:00 +0900

    [level 2] Title: [3차] 방금그곡, Time: 3.44 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,39 +1,42 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String m, String[] musicinfos) {
-        String target = normalize(m);
-        String answer = "(None)";
-        int maxDuration = -1;
-
-        for (String musicInfo : musicinfos) {
-          String[] info = musicInfo.split(",");
-          int start = toMinutes(info[0]);
-          int end = toMinutes(info[1]);
-          int duration = end - start;
-          String melody = normalize(info[3]);
-
-          StringBuilder sb = new StringBuilder();
-          for (int i = 0; i < duration / melody.length(); i++) sb.append(melody);
-          sb.append(melody, 0, duration % melody.length());
-
-          if (sb.toString().contains(target) && duration > maxDuration) {
-            maxDuration = duration;
-            answer = info[2];
-          }
-        }
-
-        return answer;
-    }
     
-    
-    int toMinutes(String hhmm) {
-        return Integer.parseInt(hhmm.substring(0, 2)) * 60 + Integer.parseInt(hhmm.substring(3, 5));
+  public String solution(String m, String[] musicinfos) {
+
+    String answer = "(None)";
+    int maxLength = -1;
+    String target = normalize(m);
+
+    for (int i = 0; i < musicinfos.length; i++) {
+      // 재생된 시간
+      String[] info = musicinfos[i].split(",");
+      String music = normalize(info[3]);
+      int duration = toMinutes(info[1]) - toMinutes(info[0]);
+
+      // 재생된 음악
+      StringBuilder sb = new StringBuilder();
+      for (int j = 0; j < duration/music.length(); j++) {
+        sb.append(music);
+      }
+      sb.append(music.substring(0, duration%music.length()));
+
+      // 포함하는지 체크 > 재생된 시간이 더 길다면 갱신
+      if (sb.toString().contains(target) && maxLength < duration) {
+          maxLength = duration;
+          answer = info[2];
+      }
     }
 
-    String normalize(String s) {
-        return s.replace("C#", "c").replace("D#", "d").replace("F#", "f")
-                .replace("G#", "g").replace("A#", "a").replace("E#", "e").replace("B#", "b");
-    }
-    
+    return answer;
+  }
+
+  public int toMinutes(String hhmm) {
+    return Integer.parseInt(hhmm.split(":")[0]) * 60 + Integer.parseInt(hhmm.split(":")[1]);
+  }
+
+  // C, C#, D, D#, E, F, F#, G, G#, A, A#, B 
+  public String normalize(String str) {
+    return str.replace("C#", "c").replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#", "a");
+  }
 }
