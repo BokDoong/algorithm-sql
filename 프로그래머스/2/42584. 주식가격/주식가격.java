df13cf77e1d28bcad0145f89cd2dd7ff3ed14e67
bokdoong <102129660+BokDoong@users.noreply.github.com>
2026-08-21 19:49:00 +0900

    [level 2] Title: 주식가격, Time: 14.37 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,43 +2,39 @@ import java.util.*;
 
 class Solution {
     
-    class Node {
-        
-        int price;
-        int idx;
-        
-        public Node(int price, int idx) {
-            this.price = price;
-            this.idx = idx;
-        }
+  public int[] solution(int[] prices) {
+   
+    int[] answer = new int[prices.length];
+
+    //  하나씩 스택에 넣는다
+    //  -> Stack<int[]> stack, 0번 : 인덱스 위치, 1번 : 원소
+    Deque<int[]> stack = new ArrayDeque<>();
+
+    for (int i = 0; i < prices.length; i++) {
+      
+      // 비면 바로 넣고      
+      if (stack.isEmpty()) {
+        stack.push(new int[]{i, prices[i]});
+        continue;
+      }
+      
+      //  top이 나보다 크면 answer의 top 인덱스에 (나의 인덱스 - top의 인덱스) + pop
+      //  top이 나보다 작거나 같을 때까지
+      while (!stack.isEmpty() && stack.peek()[1] > prices[i]) {
+        int[] val = stack.pop();
+        answer[val[0]] = i - val[0];
+      }
+      
+      // 넣고
+      stack.push(new int[]{i, prices[i]});
     }
-    
-    public int[] solution(int[] prices) {
-        
-        int[] answer = new int[prices.length];
-        Stack<Node> stack = new Stack<>();
-        
-        for (int i = 0; i < prices.length; i++) {
-            
-            Node node = new Node(prices[i], i);
-            
-            while(!stack.isEmpty()) {
-                if (stack.peek().price > prices[i]) {
-                    Node popped = stack.pop();
-                    answer[popped.idx] = i - popped.idx;
-                } else {
-                    break;
-                }
-            }
-            
-            stack.push(node);
-        }
-        
-        while(!stack.isEmpty()) {
-            Node popped = stack.pop();
-            answer[popped.idx] = prices.length - popped.idx - 1;
-        }
-    
-        return answer;
+
+    //  남아있는 애는 prices.length - 1 - 인덱스
+    while (!stack.isEmpty()) {
+      int[] val = stack.pop();
+      answer[val[0]] = prices.length - 1 - val[0];
     }
+
+    return answer;
+  }
 }
