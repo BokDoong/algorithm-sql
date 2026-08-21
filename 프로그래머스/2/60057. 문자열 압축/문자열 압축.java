290045c50e3612b16b553fed8692ed14653b6722
bokdoong <102129660+BokDoong@users.noreply.github.com>
2026-08-21 16:34:26 +0900

    [level 2] Title: 문자열 압축, Time: 3.08 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,28 +1,38 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(String s) {
-        int answer = s.length();                          // ④ 해결
-
+        
+        int answer = s.length();
+        
         for (int unit = 1; unit <= s.length() / 2; unit++) {
-            int compressed = 0;
-            String prev = s.substring(0, unit);
-            int count = 1;
-
-            for (int i = unit; i < s.length(); i += unit) {
-                String cur = s.substring(i, Math.min(i + unit, s.length()));  // ① 해결
-                if (cur.equals(prev)) count++;
-                else {
-                    compressed += prev.length() + (count > 1 ? String.valueOf(count).length() : 0);  // ③
-                    prev = cur;
-                    count = 1;
+            int tmpAnswer = 0;
+            
+            // 앞에서 부터 unit마다 잘라서 -> 리스트에 넣기
+            List<String> nums = new ArrayList<>();
+            int idx = 0;
+            while (idx < s.length()) {
+                if (idx + unit > s.length()) nums.add(s.substring(idx, s.length()));
+                else nums.add(s.substring(idx, idx+unit));
+                idx += unit;
+            }
+            
+            // 하나씩 돌아가면서 이전이랑 같은지 보기 -> 같다면 count++ -> 다르다면 count가 1이면 그대로, 1보다 크면 1+단위
+            String prev = "";
+            int cnt = 0;
+            for (String num : nums) {
+                if (num.equals(prev)) {
+                    cnt++;
+                } else {
+                    if (cnt >= 1) tmpAnswer += prev.length() + (cnt > 1 ? String.valueOf(cnt).length() : 0);
+                    prev = num;
+                    cnt = 1;
                 }
             }
-            compressed += prev.length() + (count > 1 ? String.valueOf(count).length() : 0);  // ② 마지막 런 닫기
-
-            answer = Math.min(answer, compressed);
+            tmpAnswer += prev.length() + (cnt > 1 ? String.valueOf(cnt).length() : 0);
+            answer = Math.min(answer, tmpAnswer);
         }
+        
         return answer;
     }
 }
