60394e26ad8944a079c51a52aa141358c0d72c99
bokdoong <102129660+BokDoong@users.noreply.github.com>
2026-08-29 10:12:33 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.87 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,94 +1,41 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     
-//     public void debug() {
-        
-//     }
-    
-    class Job {
-        
-        int id;
-        int requestTime;
-        int executionTime;
-        
-        public Job(int id, int requestTime, int executionTime) {
-            this.id = id;
-            this.requestTime = requestTime;
-            this.executionTime = executionTime;
-        }
-        
-        @Override
-        public String toString() {
-            return "id : " + id + ", requestTime : " + requestTime + ", executionTime : " + executionTime;
-        }
-        
-    }
-    
-    // 대기큐 최소힙 : jobs의 job들을 넣고 기다리는 최소힙, 요청들어온 시간 기준 정렬
-    public PriorityQueue<Job> waitingJobs = new PriorityQueue<>(
-        Comparator.comparingInt((Job job) -> job.requestTime)
-    );
-    // 수행큐 최소힙 : 작업 소요시간 -> 요청시각 -> 작업 번호 순으로 정렬
-    public PriorityQueue<Job> todoJobs = new PriorityQueue<>(
-        Comparator
-            .comparingInt((Job job) -> job.executionTime)
-            .thenComparingInt(job -> job.requestTime)
-            .thenComparingInt(job -> job.id)
-    );
-    
-    // jobs -> 대기큐에 넣기
-    public void initialize(int[][] jobs) {
-        for (int i = 0; i < jobs.length; i++) {
-            Job job = new Job(i, jobs[i][0], jobs[i][1]);
-            waitingJobs.add(job);
-        }
-    }
-    
-    // 1초 카운팅하며 순회 : 대기큐에서 현재 시간보다 적은 태스크들 수행큐에 넣기 -> 수행할 것 있다면 수행 + 시간 뻥튀기 & 아니라면 +1초
-    public int solve(int n) {
-        
-        int result = 0;
-        int sec = 0;
-        
-        while (!waitingJobs.isEmpty()) {
-            
-            // 지금 시간보다 요청시간이 작다면 넣기
-            while (true) {
-                
-                Job newJob = waitingJobs.peek();
-                if (newJob == null) break;
-                
-                if (newJob.requestTime <= sec) {
-                    todoJobs.add(waitingJobs.poll());
-                } else {
-                    break;
-                }
-            }
-            
-            // 할거 있다면 수행 아니면 sec + 1
-            if (!todoJobs.isEmpty()) {
-                Job todo = todoJobs.poll();
-                sec += todo.executionTime;
-                result += (sec - todo.requestTime);
-            } else {
-                sec += 1;
-            }
-        }
-        
-        // 잔존 태스크 처리
-        while (!todoJobs.isEmpty()) {
-            Job todo = todoJobs.poll();
-            sec += todo.executionTime;
-            result += (sec - todo.requestTime);
-        }
-        
-        return (int) result / n;
-    }
-    
+    // [요청 시간, 소요 시간]
     public int solution(int[][] jobs) {
-        initialize(jobs);
-        return solve(jobs.length);
+        int n = jobs.length;
+        
+        // 요청시간 기준 정렬
+        Arrays.sort(jobs, (a,b) -> a[0] - b[0]);
+        
+        // 실행후보 
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        
+        int time = 0;
+        int idx = 0;        // 도착 처리하지 않은 작업의 인덱스
+        int done = 0;
+        int total = 0;
+        
+        while (done < n) {
+            // 도착한 작업 후보에 넣기
+            while (idx < n && jobs[idx][0] <= time) {
+                pq.add(jobs[idx++]);
+            }
+            
+            // 후보 없으면 다음 시간으로
+            if (pq.isEmpty()) {
+                time = jobs[idx][0];
+                continue;
+            }
+            
+            // 가장 짧은 작업
+            int[] job = pq.poll();
+            time += job[1];
+            total += time - job[0];
+            done++;
+        }
+        
+        return total / n;
     }
 }
