558f0d1c19a741a271f540a0825ef484cbd99ce2
bokdoong <102129660+BokDoong@users.noreply.github.com>
2026-09-01 12:08:42 +0900

    [level 3] Title: 입국심사, Time: 48.86 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -5,27 +5,22 @@ class Solution {
         
         Arrays.sort(times);
         
-        // 느린 사람이 다 처리 
-        long left = 0;
+        long left = 1;
         long right = (long) times[times.length - 1] * n;
         
-        // 이분탐색
         while (left < right) {
             long mid = (left + right) / 2;
             
-            // 합 구하기
             long total = 0;
             for (int time : times) {
                 total += mid/time;
             }
             
-            // 이동
             if (total >= n) {
                 right = mid;
             } else {
                 left = mid + 1;
             }
-            
         }
         
         return left;
