40a818c9d591e9f3e228ccf8208859d0c0ebc621
bokdoong <102129660+BokDoong@users.noreply.github.com>
2026-09-03 08:25:44 +0900

    [level 3] Title: 섬 연결하기, Time: 0.80 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,44 +2,41 @@ import java.util.*;
 
 class Solution {
     
-    // 부모 노드 정보
-    int[] parents;
+    int[] parent;
     
     public int solution(int n, int[][] costs) {
+
+        // 부모
+        parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
         
-        // 부모 노드
-        parents = new int[n];
-        for (int i = 0; i < n; i++) {
-            parents[i] = i;
-        }
-        
-        // 비용 기준으로 정렬: 최대한 낮은 비용인 애들부터 갖고오기 위해
-        // 두 노드가 그룹이 되면 앞으로 해당 노드가 나오면 볼 필요가 없음
+        // 거리순으로 정렬하기
         Arrays.sort(costs, (a, b) -> a[2] - b[2]);
         
-        int answer = 0, picked = 0;
-        for (int[] e : costs) {
-            // 다른 그룹이면 합치고 더함
-            // 같은 그룹이면 이미 최소 비용을 알기 떄문에 알 필요가 없음.
-            if (find(e[0]) != find(e[1])) {
-                union(e[0], e[1]);
-                answer += e[2];
+        int picked = 0;
+        int answer = 0;
+        for (int[] cost : costs) {
+            // 다른 그룹이라면 union
+            if (find(cost[0]) != find(cost[1])) {
+                union(cost[0], cost[1]);
                 picked++;
-                if (picked == n-1) break;
+                answer += cost[2];
+            }
+            // 그룹 내 개수 체크
+            if (picked == n-1) {
+                return answer;
             }
         }
         
         return answer;
     }
     
-    // 부모 찾기
     int find(int node) {
-        if (parents[node] == node) return node;
-        return find(parents[node]);
+        if (parent[node] == node) return node;
+        return find(parent[node]);
     }
     
-    // 합치기
     void union(int node1, int node2) {
-        parents[find(node2)] = find(node1);
+        parent[find(node2)] = find(node1);
     }
 }
