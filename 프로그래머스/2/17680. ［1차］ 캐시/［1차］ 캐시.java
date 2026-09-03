d4730777c9a62181677a5bc478cbbd2d771eeab2
bokdoong <102129660+BokDoong@users.noreply.github.com>
2026-09-03 09:27:43 +0900

    [level 2] Title: [1차] 캐시, Time: 21.29 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,24 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-
-        if (cacheSize == 0) return 5 * cities.length;
-
+        
+        if (cacheSize == 0) return cities.length * 5;
+        
+        // 캐시
         Set<String> cache = new LinkedHashSet<>();
         int answer = 0;
+        
         for (String city : cities) {
-            city = city.toLowerCase();
-            if (cache.contains(city)) {
-                answer++;
-                cache.remove(city);                       // 빼고 (아래서 다시 넣어 최신화)
-            } else {
-                answer += 5;
-                if (cache.size() >= cacheSize) {
-                    cache.remove(cache.iterator().next()); // 가장 오래된 것 축출
-                }
+            // 소문자로 변환
+            String key = city.toLowerCase();
+            // 캐시에 이미 있으면 +1, 빼고 다시 넣기
+            if (cache.contains(key)) {
+                answer += 1;
+                cache.remove(key);
+                cache.add(key);
+                continue;
             }
-            cache.add(city);                              // hit/miss 공통: 맨 뒤로
+            
+            // 캐시에 없으면
+            // - 다 차있다면 맨 앞에꺼 제거
+            // - 넣기
+            if (cache.size() == cacheSize) {
+                String oldest = cache.iterator().next();
+                cache.remove(oldest);
+            }
+            cache.add(key);
+            answer += 5;
         }
+        
         return answer;
     }
 }
