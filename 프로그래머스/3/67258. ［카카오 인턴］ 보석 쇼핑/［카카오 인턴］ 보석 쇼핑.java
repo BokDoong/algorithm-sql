6f2e7d62f52c85a0f70b30ac0e5cc670822406ff
bokdoong <102129660+BokDoong@users.noreply.github.com>
2026-09-14 20:56:39 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 33.18 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -3,50 +3,56 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] gems) {
         
-        // 전체 알파벳의 종류
-        Set<String> set = new HashSet<>();
-        for (String gem : gems) {
-            set.add(gem);
-        }
-        int n = set.size();
+        // 결과
+        int maxLeft = 0;
+        int maxRight = gems.length;
         
-        // right를 0 ~ n-1 인덱스로 하나씩 늘려가며
+        // 전체 개수
+        Set<String> gemSet = new HashSet<>();
+        for (String gem : gems) {
+            gemSet.add(gem);
+        }
+        int uniqueCount = gemSet.size();
+        
+        // 포인터
         int left = 0;
-        int maxLength = Integer.MAX_VALUE, maxLeft = 0;
-        Map<String, Integer> gemCount = new HashMap<>();
-        for (int right = 0; right < gems.length; right++) {
-            
-            String gem = gems[right];
-            
-            // right의 원소를 추가
-            if (!gemCount.containsKey(gem)) {
-                gemCount.put(gem, 1);
+        int right = 0;
+        
+        Set<String> inputGemSet = new HashSet<>();
+        Map<String, Integer> inputGemCountMap = new HashMap<>();
+        while (right < gems.length) {
+            // 하나 넣고
+            String targetGem = gems[right];
+            inputGemSet.add(targetGem);
+            if (inputGemCountMap.containsKey(targetGem)) {
+                inputGemCountMap.put(targetGem, inputGemCountMap.get(targetGem) + 1);
             } else {
-                gemCount.put(gem, gemCount.get(gem) + 1);
+                inputGemCountMap.put(targetGem, 1);
             }
             
-            // 지금 추가한 원소의 종류가 전체 알파벳 종류와 같다면 + while
-            while (gemCount.size() == n) {
+            // 가득차있다면 left ++
+            while (inputGemSet.size() == uniqueCount) {
                 
-                // (right - left + 1)이 현재 길이보다 작다면 갱신
-                if (right - left + 1 < maxLength) {
-                    maxLength = right - left + 1;
+                if (right - left < maxRight - maxLeft) {
+                    maxRight = right;
                     maxLeft = left;
                 }
                 
-                // 빼기
-                String leftGem = gems[left];
-                if (gemCount.get(leftGem) == 1) {
-                    gemCount.remove(leftGem);
+                int currentCount = inputGemCountMap.get(gems[left]);
+                if (currentCount == 1) {
+                    inputGemCountMap.remove(gems[left]);
+                    inputGemSet.remove(gems[left]);
                 } else {
-                    gemCount.put(leftGem, gemCount.get(leftGem) - 1);
+                    inputGemCountMap.put(gems[left], currentCount - 1);
                 }
-                
+                                                        
                 left++;
             }
-            
+                   
+            right++;
         }
         
-        return new int[]{maxLeft + 1, maxLeft + maxLength};
+        int[] answer = {maxLeft+1, maxRight+1};
+        return answer;
     }
 }
